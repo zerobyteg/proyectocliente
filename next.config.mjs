15571b40387bbf6d26197a4c32047c8f14a18452
zerobyteg <zerobytegd@gmail.com>
2026-03-23 19:10:49 -0400

    fix: explicit basePath and assetPrefix for github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
+  basePath: isProd ? '/proyectocliente' : '',
+  assetPrefix: isProd ? '/proyectocliente' : '',
 };
 
 export default nextConfig;
